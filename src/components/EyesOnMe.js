efa39f38f96365879b53148e29044c51c493c795
Garrett <hawkpdx@icloud.com>
2024-12-30 14:58:15 -0800

    deliverables met

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,9 @@
 // Code EyesOnMe Component Here
+export default function EyesOnMe (e) {
+    
+    return (
+    <button onFocus={e => console.log('Good!')} onBlur={e => console.log('Hey! Eyes on me!')}>Eyes on me</button>
+  )
+}
+
+
